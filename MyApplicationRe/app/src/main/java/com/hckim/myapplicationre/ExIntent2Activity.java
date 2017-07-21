2972e137c45aa3723e58dedb3170aa480e998db1
Hongchul Kim <gsjune@naver.com>
2017-07-22 00:10:39 +0900

    ing2

--- MyApplicationRe/app/src/main/java/com/hckim/myapplicationre/ExIntent2Activity.java
+++ MyApplicationRe/app/src/main/java/com/hckim/myapplicationre/ExIntent2Activity.java
@@ -1,13 +1,50 @@
 package com.hckim.myapplicationre;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class ExIntent2Activity extends AppCompatActivity {
+
+    public static final int RESULT_OK = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex_intent2);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String message = intent.getStringExtra("data");
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+
     }
+
+    public void onCustomerManagementButtonClicked(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("data", "고객관리");
+        setResult(RESULT_OK, intent);
+
+        finish();
+    }
+
+    public void onSalesManagementButtonClicked(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("data", "매출관리");
+        setResult(RESULT_OK, intent);
+
+        finish();
+    }
+
+    public void onMerchandiseManagementClicked(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("data", "상품관리");
+        setResult(RESULT_OK, intent);
+
+        finish();
+    }
+
 }
--- MyApplicationRe/app/src/main/java/com/hckim/myapplicationre/ExIntentActivity.java
+++ MyApplicationRe/app/src/main/java/com/hckim/myapplicationre/ExIntentActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 
 public class ExIntentActivity extends AppCompatActivity {
 
+    public static final int REQUEST_CODE = 1000;
+    public static final int REQUEST_CODE_MANAGEMENT = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +18,20 @@ public class ExIntentActivity extends AppCompatActivity {
         findViewById(R.id.management_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String message = "android, " + "java" + 2017;
                 Intent intent = new Intent(ExIntentActivity.this, ExIntent2Activity.class);
-                startActivity(intent);
+                intent.putExtra("data", message);
+                startActivityForResult(intent, REQUEST_CODE_MANAGEMENT);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_MANAGEMENT && resultCode == RESULT_OK && data != null) {
+
+        }
     }
 }

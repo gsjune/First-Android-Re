561e3ac50c8d1d2f2fb867839cd3010430412788
Hongchul Kim <gsjune@naver.com>
2017-08-01 14:15:35 +0900

    Fragment Basketball 예제: fragment_score.xml, BasketballScoreFragment.java, activity_basketball.xml

---
+++ MyApplicationRe/app/src/main/java/com/hckim/myapplicationre/fragment/basketball/BasketballActivity.java
@@ -0,0 +1,15 @@
+package com.hckim.myapplicationre.fragment.basketball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hckim.myapplicationre.R;
+
+public class BasketballActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basketball);
+    }
+}
---
+++ MyApplicationRe/app/src/main/java/com/hckim/myapplicationre/fragment/exercise/ExerciseActivity.java
@@ -0,0 +1,15 @@
+package com.hckim.myapplicationre.fragment.exercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hckim.myapplicationre.R;
+
+public class ExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+    }
+}
